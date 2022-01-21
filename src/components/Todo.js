6ffea9cefaf90f1e6f87fd1709443487bfb75707
sandip2224 <sandipan2224@gmail.com>
2022-01-21 21:34:54 +0530

    Completed functionality updated

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,12 +5,25 @@ const Todo = ({ text, todos, setTodos, todo }) => {
         setTodos(filteredTodos)
     }
 
+    const completeHandler = () => {
+        console.log(todo)
+        const newTodos = todos.map(el => {
+            if (el.id === todo.id) {
+                return {
+                    ...el, completed: !el.completed
+                }
+            }
+            return el
+        })
+        setTodos(newTodos)
+    }
+
     return (
         <div className="todo">
-            <li className="todo-item">
+            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
                 {text}
             </li>
-            <button className='complete-btn'><i className="fas fa-check"></i></button>
+            <button className='complete-btn' onClick={completeHandler}><i className="fas fa-check"></i></button>
             <button className='trash-btn' onClick={deleteHandler}><i className="fas fa-trash"></i></button>
         </div >
     );
